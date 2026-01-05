49096296c4db27776e41a30232d197a28cd896d5
E <uyenla1224@gmail.com>
2026-01-05 15:00:48 -0800

    Improve printVersion function for error handling
    Fix version printing function to handle file errors and read lines.

--- frameworks/version.hpp
+++ frameworks/version.hpp
@@ -6,7 +6,19 @@
 #include <string>
 
 inline void printVersion() {
-  
+  std::string versionPath = "../../config/VERSION";
+  std::ifstream file(versionPath);
+
+  if (!file.is_open())
+  {
+    std::cerr << "An error happened while printing the current version.";
+  }
+
+  std::string line;
+  while (std::getline(file, line)
+  {
+    std::cout << line << "\n";
+  }
 }
 
 #endif
